dc4a68530206c867b0fe3231f46746945fb425ba
Ahsan Sium <68369475+AhsanSium@users.noreply.github.com>
2021-03-28 12:06:33 +0600

    first commit

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB150MdpsHDnRTE1ZNUDR3x9u8_w_RJ_qo",
+    authDomain: "burj-al-arab-15594.firebaseapp.com",
+    projectId: "burj-al-arab-15594",
+    storageBucket: "burj-al-arab-15594.appspot.com",
+    messagingSenderId: "1049784957361",
+    appId: "1:1049784957361:web:4bda3785cf62346b7ce590"
+  };
+
+export default firebaseConfig;
